6999c2cb984eafcb5034716def0ae9f6d1fd8f30
Mohd Zaid <[razvizaid259@gmail.com]>
2025-06-28 23:08:39 +0530

    adds prompt and user models

---
+++ src/lib/models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+  username: { type: String, required: true },
+  email: { type: String, required: true , unique: true },
+  password: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+export const UserModel: Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', UserSchema)
