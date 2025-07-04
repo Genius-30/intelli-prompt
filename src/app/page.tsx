1a2e208f7f422bc62e19195c80ccc2700a7cae01
Genius30 <geniusporwal01@gmail.com>
2025-07-04 15:57:26 +0530

    FInal routing done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+<<<<<<< HEAD
 import Hero from "@/components/hero";
+=======
+import { Hero } from "@/components/hero";
+import { SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const { userId } = await auth();
+
+  // ✅ Redirect to dashboard if user is already signed in
+  if (userId) {
+    redirect("/dashboard");
+  }
+>>>>>>> 2fafa30 (FInal routing done)
+
   return (
-    <main>
-      <Hero />
-    </main>
+    <SignedOut>
+      <main>
+        <Hero />
+      </main>
+    </SignedOut>
   );
 }
