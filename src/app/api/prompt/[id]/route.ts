7397ba5b449f9ad76fe76442b57856713f20873a
Mohd Zaid <[razvizaid259@gmail.com]>
2025-07-07 14:43:47 +0530

    adds route to get a specific prompt

--- src/app/api/prompt/[id]/route.ts
+++ src/app/api/prompt/[id]/route.ts
@@ -23,3 +23,24 @@ export async function DELETE(
     return NextResponse.json({ error: "err deleting prompt" }, { status: 500 });
   }
 }
+
+export async function GET(
+  req: NextRequest, 
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { userId, error } = await getAuthenticatedUser()
+    if(error) return error
+
+    const promptId = (await params).id
+    if(!mongoose.Types.ObjectId.isValid(promptId)) {
+      return NextResponse.json({ message: 'invalid promptId' },{ status: 400 })
+    }
+
+    const prompt = await Prompt.findById({ _id: promptId })
+
+    return NextResponse.json({ message: "prompt fetchec", prompt }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ error: "err fetching prompt" }, { status: 500 });
+  }
+}
